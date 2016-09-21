15ebd9a450b2fe3717977c8767c838f84fcab3f7
Jacob Brooker <jacob.brooker@dawsoncollege.qc.ca>
2016-09-21 16:52:59 -0400

    Finished

--- app/src/main/java/com/example/a1439510/countrydisplay/MainActivity.java
+++ app/src/main/java/com/example/a1439510/countrydisplay/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a1439510.countrydisplay;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openMap(View view){
+        Intent map = new Intent(Intent.ACTION_VIEW);
+        map.setData(Uri.parse("geo:0,0?q="+ getString(R.string.country)));
+        if(map.resolveActivity(getPackageManager()) != null) {
+            startActivity(map);
+        }
+    }
+
 }
