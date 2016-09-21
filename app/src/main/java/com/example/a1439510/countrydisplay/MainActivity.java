23567f0db98917cc65124fa9ff1c8686b6949663
Jacob Brooker <jacob.brooker@dawsoncollege.qc.ca>
2016-09-21 14:51:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1439510/countrydisplay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1439510.countrydisplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
